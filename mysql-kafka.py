e0027dfd7a976ece4c8798a27f28926e4ecedf57
Sangeetha Prabhagaran <sangeethaprabhagaran@gmail.com>
2022-10-27 15:15:13 +0530

    Update mysql-kafka.py
    Added comments for better understanding

--- mysql-kafka.py
+++ mysql-kafka.py
@@ -3,16 +3,20 @@ import mysql.connector
 from time import sleep
 from json import dumps
 
-connection = mysql.connector.connect(host="localhost",database="development",user="root",password="Trellis#1")
+#create a mysql connection
+connection = mysql.connector.connect(host="localhost",database="development",user="****",password="******")
 
+#create a curso to fetch all the records from mysql table employee
 cursor = connection.cursor()
 cursor.execute("select * from employee")
 record = cursor.fetchall()
 cursor.close()
 connection.close()
 
+#create an instance of th KafkaProducer connecting to local kafka server and JSON value serializer
 producer = KafkaProducer(bootstrap_servers=["localhost:9092"],value_serializer= lambda m: dumps(m).encode('utf-8'))
 
+#Loop through the records and set the data to be a json data and send the son data to kafka topic employee
 for i in record:
 	data={"id":i[0],"name":i[1],"age":i[2],"yoe":i[3],"salary":i[4]}
 	producer.send("employee",data)
